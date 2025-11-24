b38861315913b903fd087e85ef0c50842f9b806c
MEO265 <99362508+MEO265@users.noreply.github.com>
2025-11-24 08:22:38 +0100

    Add first github actions

--- R/test.R
+++ R/test.R
@@ -2,4 +2,4 @@
 #' @param x Integer (0 or 1)
 #' @param y Integer (0 or 1)
 #' @returns Integer (0 or 1)
-test <- function (x, y) .Call(c_run_cudd_example, x, y)
+test <- function(x, y) .Call(c_run_cudd_example, x, y)

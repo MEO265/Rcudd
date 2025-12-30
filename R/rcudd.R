75a35585bf891b555a626d5c94172bd357c98f03
MEO265 <99362508+MEO265@users.noreply.github.com>
2025-12-30 09:21:15 +0100

    Resolve native symbols before calling

--- R/rcudd.R
+++ R/rcudd.R
@@ -1,8 +1,22 @@
 #' @useDynLib Rcudd, .registration=TRUE
 NULL
 
+.rcudd_symbols <- new.env(parent = emptyenv())
+
+.rcudd_symbol <- function(name) {
+  if (exists(name, envir = .rcudd_symbols, inherits = FALSE)) {
+    return(get(name, envir = .rcudd_symbols, inherits = FALSE))
+  }
+  addr <- getNativeSymbolInfo(name, PACKAGE = "Rcudd")$address
+  assign(name, addr, envir = .rcudd_symbols)
+  return(addr)
+}
+
 .rcudd_call <- function(symbol, ...) {
-  .Call(symbol, ..., PACKAGE = "Rcudd")
+  if (is.character(symbol)) {
+    symbol <- .rcudd_symbol(symbol)
+  }
+  .Call(symbol, ...)
 }
 
 .cudd_check_same_manager <- function(lhs, rhs, op) {
